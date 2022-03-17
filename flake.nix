afc4b14a50884a7f3df7de9a869f06c3874792c5
samrose <samuel.rose@gmail.com>
2022-03-17 13:14:35 -0400

    FLXKE

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    neovim-with-config = pkgs.neovim.override {
+      configure = {
+        customRC = ''
+          set title
+          set nu
+          autocmd BufWritePre * :%s/\s\+$//e
+        '';
+        packages.package.start = with pkgs.vimPlugins; [
+          vim-nix
+          vim-parinfer
+          YouCompleteMe
+        ];
+      };
+
+      viAlias = true;
+      withPython3 = true;
+      withRuby = false;
+    };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [neovim-with-config pkgs.alejandra pkgs.bat pkgs.jq pkgs.nix-eval-jobs];
+      shellHook = ''
+        export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+        export BAT_THEME="Solarized (light)"
+      '';
+    };
+  };
+}
