2563ed0860fa6c57fd6debef55756730bdae8d99
samrose <samuel.rose@gmail.com>
2022-04-04 14:53:17 -0400

    defaultPackage

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
       withRuby = false;
     };
   in {
+    defaultPackage.x86_64-linux = "flxke";
     devShell.x86_64-linux = pkgs.mkShell {
       buildInputs = [neovim-with-config pkgs.alejandra pkgs.bat pkgs.jq];
       shellHook = ''
