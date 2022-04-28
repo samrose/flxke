485a56b44711ef0b76e0dceb9ca43fe2ae8f1450
samrose <samuel.rose@gmail.com>
2022-04-28 09:04:31 -0400

    refactor for go

--- flake.nix
+++ flake.nix
@@ -1,51 +1,57 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
-  };
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    neovim-with-config = pkgs.neovim.override {
-      configure = {
-        customRC = ''
-          set title
-          set nu
-          autocmd BufWritePre * :%s/\s\+$//e
-          set expandtab
-          set tabstop=2
-          set softtabstop=2
-          set shiftwidth=2
-        '';
-        packages.package.start = with pkgs.vimPlugins; [
-          vim-go
-          vim-nix
-          vim-parinfer
-          YouCompleteMe
-        ];
-      };
+  description = "flxke go lang";
 
-      viAlias = true;
-      withPython3 = true;
-      withRuby = false;
-    };
-  in {
-    packages.x86_64-linux.default = pkgs.buildEnv {
-      name = "flxke";
-      paths = [neovim-with-config pkgs.alejandra pkgs.bat pkgs.jq];
-    };
-    apps.x86_64-linux.default = {
-      type = "app";
-      program =
-        (pkgs.writeShellApplication {
-          name = "activate";
-          text = ''
-            export BAT_THEME="Solarized (light)"
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "github:flox/nixpkgs/unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      devShell = forAllSystems (system:
+       let pkgs = nixpkgsFor.${system};
+      neovim-with-config = pkgs.neovim.override {
+        configure = {
+          customRC = ''
+            set title
+            set nu
+            autocmd BufWritePre * :%s/\s\+$//e
+            set expandtab
+            set tabstop=2
+            set softtabstop=2
+            set shiftwidth=2
           '';
-        })
-        .outPath
-        + "/bin/activate";
+          packages.package.start = with pkgs.vimPlugins; [
+            pkgs.fzf
+            vim-go
+            vim-nix
+            vim-parinfer
+            YouCompleteMe
+          ];
+        };
+        viAlias = true;
+        withPython3 = true;
+        withRuby = false;
+      };
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [ alejandra bat bats  jq  go_1_18 gopls  gofumpt neovim-with-config python3 tmux ];
+      });
     };
-  };
 }
